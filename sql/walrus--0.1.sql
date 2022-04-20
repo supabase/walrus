974798432bbb21d4838efa4ad3de2dd95f2b7edc
Oliver Rice <github@oliverrice.com>
2022-04-20 06:24:38 -0500

    support quoted regtypes

--- sql/walrus--0.1.sql
+++ sql/walrus--0.1.sql
@@ -194,7 +194,8 @@ $$;
 
 create type realtime.wal_column as (
     name text,
-    type text,
+    type_name text,
+    type_oid oid,
     value jsonb,
     is_pkey boolean,
     is_selectable boolean
@@ -258,7 +259,7 @@ Should the record be visible (true) or filtered out (false) after *filters* are 
             sum(
                 realtime.check_equality_op(
                     op:=f.op,
-                    type_:=col.type::regtype,
+                    type_:=col.type_oid::regtype,
                     -- cast jsonb to text
                     val_1:=col.value #>> '{}',
                     val_2:=f.value
@@ -332,7 +333,11 @@ begin
             (
                 x->>'name',
                 x->>'type',
-                realtime.cast((x->'value') #>> '{}', (x->>'type')::regtype),
+                x->>'typeoid',
+                realtime.cast(
+                    (x->'value') #>> '{}',
+                    (x->>'typeoid')::regtype
+                ),
                 (pks ->> 'name') is not null,
                 true
             )::realtime.wal_column
@@ -347,7 +352,11 @@ begin
             (
                 x->>'name',
                 x->>'type',
-                realtime.cast((x->'value') #>> '{}', (x->>'type')::regtype),
+                x->>'typeoid',
+                realtime.cast(
+                    (x->'value') #>> '{}',
+                    (x->>'typeoid')::regtype
+                ),
                 (pks ->> 'name') is not null,
                 true
             )::realtime.wal_column
@@ -364,7 +373,8 @@ begin
             array_agg(
                 (
                     c.name,
-                    c.type,
+                    c.type_name,
+                    c.type_oid,
                     c.value,
                     c.is_pkey,
                     pg_catalog.has_column_privilege(working_role, entity_, c.name, 'SELECT')
@@ -377,7 +387,8 @@ begin
                 array_agg(
                     (
                         c.name,
-                        c.type,
+                        c.type_name,
+                        c.type_oid,
                         c.value,
                         c.is_pkey,
                         pg_catalog.has_column_privilege(working_role, entity_, c.name, 'SELECT')
---
+++ test/sql/issue_40_quoted_regtype.sql
@@ -0,0 +1,39 @@
+/*
+Tests that, regtypes that require quoting are handled without exception
+*/
+
+select 1 from pg_create_logical_replication_slot('realtime', 'wal2json', false);
+
+
+create type "Color" as enum ('RED', 'YELLOW', 'GREEN');
+
+create table public.notes(
+    id int primary key,
+    primary_color "Color"
+);
+
+insert into realtime.subscription(subscription_id, entity, claims, filters)
+select
+    seed_uuid(1),
+    'public.notes',
+    jsonb_build_object(
+        'role', 'authenticated',
+        'sub', seed_uuid(2)::text
+    ),
+    array[('primary_color', 'eq', 'RED')::realtime.user_defined_filter];
+
+insert into public.notes(id, primary_color)
+values
+    (1, 'RED'),   -- matches filter
+    (2, 'GREEN'); -- does not match filter
+
+select
+    rec,
+    is_rls_enabled,
+    subscription_ids,
+    errors
+from
+   walrus;
+
+
+
