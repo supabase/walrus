bd550e544833fd8416e4f7b4a85fe261f1322c3a
Oliver Rice <github@oliverrice.com>
2022-09-08 11:20:59 -0500

    handle null values in filters. Null fails closed

--- test/sql/test_unit_filters.sql
+++ test/sql/test_unit_filters.sql
@@ -16,6 +16,7 @@ select realtime.check_equality_op('gte', 'text', 'bbb', 'aaa');
 select realtime.check_equality_op('gte', 'text', 'bbb', 'bbb');
 select realtime.check_equality_op('eq', 'bigint', '1', '1');
 select realtime.check_equality_op('eq', 'bigint', '2', '1');
+select realtime.check_equality_op('eq', 'bigint', '2', null);
 
 select realtime.check_equality_op('eq','uuid','639f86b1-738b-43ed-bb09-c8d3f3bafa30','639f86b1-738b-43ed-bb09-c8d3f3bafa30');
 select realtime.check_equality_op('eq','uuid','639f86b1-738b-43ed-bb09-c8d3f3bafa30','b423f213-ac24-402a-95ea-cf1d94d8e9f0');
